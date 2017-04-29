f77a1f75a57efdfedd5503ff45398b9cb9c18679
sunoko <staybesidehangon@gmail.com>
2017-04-29 15:16:22 +0900

    a

--- container_01/test/controllers/static_pages_controller_test.rb
+++ container_01/test/controllers/static_pages_controller_test.rb
@@ -6,15 +6,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails on Docker"
   end
 
-  def test_should_get_root
-    get static_pages_home_url
+  test "should get root" do
+    get root_path
     assert_response :success
-  end
-  
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
