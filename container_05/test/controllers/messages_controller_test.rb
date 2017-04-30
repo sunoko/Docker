98d484b63bdb8095911e4476e5dd33d579e50d1b
sunoko <staybesidehangon@gmail.com>
2017-05-01 03:25:22 +0900

    update test

--- container_05/test/controllers/messages_controller_test.rb
+++ container_05/test/controllers/messages_controller_test.rb
@@ -1,9 +1,24 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get messages_index_url
+  def setup
+    @message = messages(:one)
+  end
+  
+  test "should get root" do
+    get root_path
     assert_response :success
   end
 
+  test "should get index" do
+    get messages_url
+    assert_response :success
+  end
+
+  test "should post create" do
+    assert_difference 'Message.count', 1 do
+      post messages_path, message: { name: 'taro',
+                                  body: 'hello'}
+    end
+  end 
 end
